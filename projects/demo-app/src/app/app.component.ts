e983d5ac50fd13f6d0f1670d93c6f06be927a117
Kevin Latusinski <me@kevlatus.de>
2020-12-17 14:49:47 +0100

    demo: showcase declarative approach

--- projects/demo-app/src/app/app.component.ts
+++ projects/demo-app/src/app/app.component.ts
@@ -1,15 +1,8 @@
 import { Component } from '@angular/core';
-import { BottomNavItem } from 'ngx-bottom-nav';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  items: BottomNavItem[] = [
-    { icon: 'home', label: 'Home', routerLink: '', exact: true },
-    { icon: 'search', label: 'Search', routerLink: 'search' },
-    { icon: 'forum', label: 'Forum', routerLink: 'forum' },
-  ];
-}
+export class AppComponent {}
