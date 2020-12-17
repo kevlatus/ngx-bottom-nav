354b1b888923a0db035d95df4cee6f17f123c0bd
Kevin Latusinski <me@kevlatus.de>
2020-12-17 14:49:26 +0100

    feat: move routerLink definition to button component

--- projects/ngx-bottom-nav/src/lib/bottom-nav-button/bottom-nav-button.component.ts
+++ projects/ngx-bottom-nav/src/lib/bottom-nav-button/bottom-nav-button.component.ts
@@ -1,15 +1,17 @@
 import { Component, Input, HostBinding } from '@angular/core';
 
+import { BottomNavItem } from '../bottom-nav';
+
 @Component({
   selector: 'button[ngx-bottom-nav]',
   templateUrl: './bottom-nav-button.component.html',
-  styleUrls: ['./bottom-nav-button.component.scss']
+  styleUrls: ['./bottom-nav-button.component.scss'],
 })
-export class BottomNavButtonComponent {
+export class BottomNavButtonComponent implements BottomNavItem {
   /**
    * Determines whether labels of inactive buttons are hidden.
    */
-  @HostBinding('class.label-hidden') @Input() public hideLabel: boolean;
+  @Input() public hideLabel: boolean;
 
   /**
    * The icon which is shown on this button.
@@ -20,4 +22,8 @@ export class BottomNavButtonComponent {
    * The label which is shown on this button.
    */
   @Input() public label: string;
+
+  @Input() public routerLink: string;
+
+  @Input() public exact?: boolean;
 }
